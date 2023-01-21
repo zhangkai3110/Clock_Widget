ff3d4998d5b0fa359ee0c8d4c2932b415fbeed43
QBeukelman <98453355+QBeukelman@users.noreply.github.com>
2023-01-21 19:33:38 +0100

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Quentin Beukelman on 21/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
